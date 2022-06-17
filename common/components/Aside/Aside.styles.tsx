281d973ee32c8a886c4bde8eb8d8ac43d381c8f3
MaxZay <maksim.zaitsau@innowise-group.com>
2022-06-17 15:33:17 +0300

    feat: add aside component

---
+++ common/components/Aside/Aside.styles.tsx
@@ -0,0 +1,10 @@
+import {Grid, styled} from "@mui/material";
+
+export const Styles = {
+  Wrapper: styled(Grid)`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  `
+}
---
+++ common/components/Aside/Aside.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Styles } from './Aside.styles'
+
+const Aside = () => {
+  return (
+    <Styles.Wrapper>
+
+    </Styles.Wrapper>
+  );
+};
+
+export default Aside;
