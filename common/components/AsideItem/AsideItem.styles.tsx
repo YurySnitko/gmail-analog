7cd181b093451d6a17594b189afe5ad56d126569
MaxZay <maximzay2017@gmail.com>
2022-06-20 20:26:58 +0300

    fix: correct aside list item styles

--- common/components/AsideItem/AsideItem.styles.tsx
+++ common/components/AsideItem/AsideItem.styles.tsx
@@ -9,10 +9,16 @@ export const S = {
   ListButton: styled(ListItemButton)`
     border-radius: 0 20px 20px 0;
     padding: 0 0 0 26px;
+    display: flex;
+    gap: 1rem;
+    
+    & > div {
+      min-width: 0;
+    }
   `,
 
   ItemTitle: styled(Typography)`
-    line-height: 1.8rem;
+    line-height: 2rem;
     font-size: 0.875rem;
     letter-spacing: .2px;
   `
