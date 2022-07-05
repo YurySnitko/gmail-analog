5a6c17e963ddc93ff81a800988b00abde6f5f72b
MaxZay <maximzay2017@gmail.com>
2022-07-05 19:59:15 +0300

    feat: added mails from redux in list component

--- src/common/components/IncomingMails/IncomingMails.tsx
+++ src/common/components/IncomingMails/IncomingMails.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import MailListHeader from '../../ui-kit/MailListHeader/MailListHeader';
 import MailList from '../../ui-kit/MailList/MailList';
-import { mails } from '../../../../consts/mails';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux.hook';
 import { getMailsFetch } from '../../../store/reducers/MailsSlice';
+import { CircularProgress } from '@mui/material';
+import { S } from './IncomingMails.styles';
 
 const IncomingMails = () => {
   const [selectedMailsIds, setSelectedMailsIds] = useState<string[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const test = useAppSelector((state) => state.mailReducer);
+  const { mails, isLoading } = useAppSelector((state) => state.mailReducer);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getMailsFetch());
   }, [dispatch]);
-
-  console.log(test);
 
   const setSelectedMailsIdsHandler = (ids: string[]) => {
     setSelectedMailsIds(ids);
@@ -33,12 +32,18 @@ const IncomingMails = () => {
         currentPage={currentPage}
         setCurrentPage={setCurrentPageHandler}
       />
-      <MailList
-        mailList={mails}
-        selectedMailsIds={selectedMailsIds}
-        setSelectedMailsIds={setSelectedMailsIdsHandler}
-        currentPage={currentPage}
-      />
+      {isLoading ? (
+        <S.LoaderWrapper>
+          <CircularProgress />
+        </S.LoaderWrapper>
+      ) : (
+        <MailList
+          mailList={mails}
+          selectedMailsIds={selectedMailsIds}
+          setSelectedMailsIds={setSelectedMailsIdsHandler}
+          currentPage={currentPage}
+        />
+      )}
     </div>
   );
 };
