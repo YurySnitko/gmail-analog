f7bfc385cb63ea0626ae560b487493c3b8ff950d
MaxZay <maksim.zaitsau@innowise-group.com>
2022-08-23 14:38:17 +0300

    fix: deleted useless code

--- src/common/components/Header/Header.test.jsx
+++ src/common/components/Header/Header.test.jsx
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Header } from './Header';
 import Helper from '../../../hocs/TestHelper/Helper';
-import { useRouter } from 'next/router';
 import '@testing-library/jest-dom';
 
 jest.mock('next/router', () => ({
@@ -18,17 +17,6 @@ jest.mock('next/router', () => ({
 jest.spyOn(require('next/router'), 'useRouter');
 
 describe('Header', () => {
-  useRouter.mockImplementation(() => ({
-    useRouter() {
-      return {
-        route: '/',
-        pathname: '',
-        query: '',
-        asPath: '',
-      };
-    },
-  }));
-
   it('Test', () => {
     render(
       <Helper>
